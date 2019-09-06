13824286dbc93ed89344b6528d28da3d44cde40e
Shivam Mathur <shivam_jpr@hotmail.com>
2019-09-06 20:25:07 +0530

    Install composer manually

--- src/darwin.sh
+++ src/darwin.sh
@@ -2,8 +2,11 @@ echo $1
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew unlink php
 brew tap exolnet/homebrew-deprecated
+brew tap homebrew/homebrew-php
 brew install php@$1
 brew link --force --overwrite php@$1
-brew install composer
+curl -sS https://getcomposer.org/installer | php
+chmod +x composer.phar
+mv composer.phar /usr/local/bin/composer
 php -v
 composer -V

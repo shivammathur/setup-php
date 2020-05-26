b84dd3ce2517573c64219c4697d6758295133569
Shivam Mathur <shivam_jpr@hotmail.com>
2020-05-26 06:41:18 +0530

    Remove empty line in logs

--- src/scripts/linux.sh
+++ src/scripts/linux.sh
@@ -403,6 +403,6 @@ ext_dir=$(php -i | grep "extension_dir => /" | sed -e "s|.*=> s*||")
 scan_dir=$(php --ini | grep additional | sed -e "s|.*: s*||")
 ini_file=$(php --ini | grep "Loaded Configuration" | sed -e "s|.*:s*||" | sed "s/ //g")
 pecl_file="$scan_dir"/99-pecl.ini
-echo '' | sudo tee "$pecl_file"
+echo '' | sudo tee "$pecl_file" >/dev/null 2>&1
 sudo chmod 777 "$ini_file" "$pecl_file" "$tool_path_dir"
 add_log "$tick" "PHP" "$status PHP $semver"

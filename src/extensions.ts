a094f682be8b2542570b3351124ef230a1f8f2a7
Shivam Mathur <shivam_jpr@hotmail.com>
2020-11-16 18:05:08 +0530

    Add support for couchbase

--- src/extensions.ts
+++ src/extensions.ts
@@ -27,12 +27,14 @@ export async function addExtensionDarwin(
       // match pdo_oci and oci8
       // match 5.3ioncube...7.4ioncube, 7.0ioncube...7.4ioncube
       // match 7.0phalcon3...7.3phalcon3 and 7.2phalcon4...7.4phalcon4
+      // match 5.6couchbase...7.4couchbase
       case /^(5\.[3-6]|7\.[0-4])blackfire(-\d+\.\d+\.\d+)?$/.test(
         version_extension
       ):
       case /^pdo_oci$|^oci8$/.test(extension):
       case /^5\.[3-6]ioncube$|^7\.[0-4]ioncube$/.test(version_extension):
       case /^7\.[0-3]phalcon3$|^7\.[2-4]phalcon4$/.test(version_extension):
+      case /^5\.6couchbase$|^7\.[0-4]couchbase$/.test(version_extension):
         add_script += await utils.customPackage(
           ext_name,
           'ext',
@@ -219,7 +221,7 @@ export async function addExtensionLinux(
       // match pdo_oci and oci8
       // match 5.3ioncube...7.4ioncube, 7.0ioncube...7.4ioncube
       // match 7.0phalcon3...7.3phalcon3 and 7.2phalcon4...7.4phalcon4
-      // match 5.6gearman..7.4gearman
+      // match 5.6gearman...7.4gearman, 5.6couchbase...7.4couchbase
       case /^(5\.[3-6]|7\.[0-4])blackfire(-\d+\.\d+\.\d+)?$/.test(
         version_extension
       ):
@@ -232,7 +234,7 @@ export async function addExtensionLinux(
       ):
       case /^5\.[3-6]ioncube$|^7\.[0-4]ioncube$/.test(version_extension):
       case /^7\.[0-3]phalcon3$|^7\.[2-4]phalcon4$/.test(version_extension):
-      case /^((5\.6)|(7\.[0-4]))gearman$/.test(version_extension):
+      case /^((5\.6)|(7\.[0-4]))(gearman|couchbase)$/.test(version_extension):
         add_script += await utils.customPackage(
           ext_name,
           'ext',

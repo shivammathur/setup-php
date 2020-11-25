24ac87e100f8bea6daed892f7a8789069bdb9e57
Shivam Mathur <shivam_jpr@hotmail.com>
2020-11-25 22:46:59 +0530

    Improve darwin.sh

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -2,7 +2,6 @@
 export tick="✓"
 export cross="✗"
 export curl_opts=(-sL)
-export nightly_versions="8.[0-1]"
 export old_versions="5.[3-5]"
 export tool_path_dir="/usr/local/bin"
 export composer_bin="$HOME/.composer/vendor/bin"

f8646352c6cac4b216ffa80ed70cf30858a822f4
Shivam Mathur <shivam_jpr@hotmail.com>
2021-03-06 01:37:12 +0530

    Fix gearman setup

--- src/scripts/ext/gearman.sh
+++ src/scripts/ext/gearman.sh
@@ -1,17 +1,16 @@
 # Helper function to add gearman extension.
 add_gearman_helper() {
-  sudo "${debconf_fix:?}" add-apt-repository ppa:ondrej/pkg-gearman -y
-  if [ -e "${ext_dir:?}/gearman.so" ] && [ "$DISTRIB_RELEASE" != "16.04" ]; then
-    ${apt_install:?} libgearman-dev
-    echo "extension=gearman.so" | sudo tee -a "${scan_dir:?}/20-gearman.ini" >/dev/null 2>&1
-  else
+  add_ppa ondrej/pkg-gearman
+  install_packages libgearman-dev
+  enable_extension gearman extension
+  if ! check_extension gearman; then
     status="Installed and enabled"
-    if [ "$DISTRIB_RELEASE" = "16.04" ]; then
-      sudo "${debconf_fix:?}" apt-get update -y
-      ${apt_install:?} php"${version:?}"-gearman
+    if [[ "${version:?}" =~ 5.[3-5] ]]; then
+      pecl_install gearman-1.1.2
     else
-      ${apt_install:?} libgearman-dev php"${version:?}"-gearman
+      install_packages php"${version:?}"-gearman || pecl_install gearman
     fi
+    enable_extension gearman extension
   fi
 }
 

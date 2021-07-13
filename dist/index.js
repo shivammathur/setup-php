3b2ce6cae64bceb25529e7db7584a066c8481d0e
Shivam Mathur <shivam_jpr@hotmail.com>
2021-07-13 14:21:36 +0530

    Update Ubuntu 16.04 warning in install.ts

--- dist/index.js
+++ dist/index.js
@@ -440,7 +440,7 @@ exports.getScript = getScript;
 async function run() {
     try {
         if ((await utils.readEnv('ImageOS')) == 'ubuntu16') {
-            core.warning('Ubuntu 16.04 is deprecated.\nPlease upgrade to Ubuntu 18.04 or Ubuntu 20.04 - https://setup-php.com/i/452');
+            core.warning('setup-php will stop working on Ubuntu 16.04 from August 1, 2021. Please upgrade to Ubuntu 18.04 or Ubuntu 20.04 - https://setup-php.com/i/452');
         }
         const version = await utils.parseVersion(await utils.getInput('php-version', true));
         if (version) {

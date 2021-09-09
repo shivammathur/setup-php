64e9bb0e7926d461758129de2a94e57f8aeebe11
Shivam Mathur <shivam_jpr@hotmail.com>
2021-09-09 07:19:49 +0530

    Add special cases to config.test.ts

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -6,6 +6,9 @@ describe('Config tests', () => {
     ${'a=b, c=d'} | ${'win32'}   | ${'Add-Content "$php_dir\\php.ini" "a=b\nc=d"'}
     ${'a=b, c=d'} | ${'linux'}   | ${'echo "a=b\nc=d" | sudo tee -a "${pecl_file:-${ini_file[@]}}"'}
     ${'a=b, c=d'} | ${'darwin'}  | ${'echo "a=b\nc=d" | sudo tee -a "${pecl_file:-${ini_file[@]}}"'}
+    ${'a=b & ~c'} | ${'win32'}   | ${'Add-Content "$php_dir\\php.ini" "a=\'b & ~c\'"'}
+    ${'a="~(b)"'} | ${'win32'}   | ${'Add-Content "$php_dir\\php.ini" "a=\'~(b)\'"'}
+    ${'a="b, c"'} | ${'win32'}   | ${'Add-Content "$php_dir\\php.ini" "a=b, c"'}
     ${'a=b, c=d'} | ${'openbsd'} | ${'Platform openbsd is not supported'}
   `(
     'checking addINIValues on $os_version',

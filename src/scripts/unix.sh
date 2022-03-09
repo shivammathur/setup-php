035c0a8550db4ebc71ecf69611f73c2909aa98ba
Shivam Mathur <shivam_jpr@hotmail.com>
2022-03-10 04:53:12 +0530

    Use local variables in log functions

--- src/scripts/unix.sh
+++ src/scripts/unix.sh
@@ -21,15 +21,15 @@ fi
 
 # Function to log start of a operation.
 step_log() {
-  message=$1
+  local message=$1
   printf "\n\033[90;1m==> \033[0m\033[37;1m%s\033[0m\n" "$message"
 }
 
 # Function to log result of a operation.
 add_log() {
-  mark=$1
-  subject=$2
-  message=$3
+  local mark=$1
+  local subject=$2
+  local message=$3
   if [ "$mark" = "$tick" ]; then
     printf "\033[32;1m%s \033[0m\033[34;1m%s \033[0m\033[90;1m%s\033[0m\n" "$mark" "$subject" "$message"
   else

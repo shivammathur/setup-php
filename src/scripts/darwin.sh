209324abab04ce351f602d0df528bf4754737967
Shivam Mathur <shivam_jpr@hotmail.com>
2022-05-05 23:43:11 +0530

    Prefix extension formula with tap name on macOS

--- src/scripts/darwin.sh
+++ src/scripts/darwin.sh
@@ -95,7 +95,7 @@ add_brew_extension() {
     sudo mv "$tap_dir"/"$ext_tap"/.github/deps/"$formula"/* "$core_repo/Formula/" 2>/dev/null || true
     update_dependencies >/dev/null 2>&1
     disable_dependency_extensions "$extension" >/dev/null 2>&1
-    brew install -f "$formula@$version" >/dev/null 2>&1
+    brew install -f "$ext_tap/$formula@$version" >/dev/null 2>&1
     copy_brew_extensions "$formula"
     add_extension_log "$extension" "Installed and enabled"
   fi

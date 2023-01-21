1267cf80a135b61e891e565aebe30f6e41756dba
Shivam Mathur <shivam_jpr@hotmail.com>
2023-01-22 02:10:29 +0530

    Change choco install script URL

--- src/scripts/extensions/firebird.ps1
+++ src/scripts/extensions/firebird.ps1
@@ -4,7 +4,7 @@ Function Add-Choco() {
       # Source: https://docs.chocolatey.org/en-us/choco/setup
       Set-ExecutionPolicy Bypass -Scope Process -Force
       [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
   } catch { }
 }

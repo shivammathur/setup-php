bac56c8d91e113006118e73cd36b3fda47b3842b
Shivam Mathur <shivam_jpr@hotmail.com>
2023-11-27 15:47:27 +0530

    Add patch for geos

--- src/scripts/extensions/patches/geos.sh
+++ src/scripts/extensions/patches/geos.sh
@@ -3,5 +3,7 @@ patch_geos() {
     sed -i~ -e "s/, ce->name/, ZSTR_VAL(ce->name)/; s/ulong /zend_ulong /" geos.c
   fi
   get -q -n /tmp/php8.patch https://git.remirepo.net/cgit/rpms/php/php-geos.git/plain/0003-add-all-arginfo-and-fix-build-with-PHP-8.patch
+  get -q -n /tmp/toString.patch https://git.remirepo.net/cgit/rpms/php/php-geos.git/plain/0006-fix-__toString-with-8.2.patch
   patch -p1 < /tmp/php8.patch 2>/dev/null || true
+  patch -p1 < /tmp/toString.patch 2>/dev/null || true
 }

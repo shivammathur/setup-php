1874353919e0b55f0a4c2839ef3dababc0df2dc7
Shivam Mathur <shivam_jpr@hotmail.com>
2023-11-27 17:08:32 +0530

    Fix linking grpc on macos but installed along protoc

--- src/scripts/tools/grpc_php_plugin.sh
+++ src/scripts/tools/grpc_php_plugin.sh
@@ -24,8 +24,10 @@ get_grpc_tag() {
 add_grpc_php_plugin_brew() {
   . "${0%/*}"/tools/brew.sh
   configure_brew
+  [ -e /usr/local/bin/protoc ] && sudo mv /usr/local/bin/protoc /tmp/protoc && sudo mv /usr/local/include/google /tmp
   brew install grpc
   brew link --force --overwrite grpc >/dev/null 2>&1
+  [ -e /tmp/protoc ] && sudo mv /tmp/protoc /usr/local/bin/protoc && sudo mv /tmp/google /usr/local/include/
   grpc_tag="v$(brew info grpc | grep "grpc:" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")"
   license_path="$(brew --prefix grpc)/LICENSE"
 }

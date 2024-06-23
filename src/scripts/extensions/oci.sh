606ac25b8439b7f029cd0978e1e776843d7bd400
Shivam Mathur <shivam_jpr@hotmail.com>
2024-06-23 07:54:02 +0530

    Fix oci extension support on Ubuntu 24.04

--- src/scripts/extensions/oci.sh
+++ src/scripts/extensions/oci.sh
@@ -30,6 +30,10 @@ add_client() {
       sudo mv "$icdir"/* "$oracle_client"/
     done
     sudo ln -sf /opt/oracle/instantclient/*.$lib_ext* $libs
+    if [ "$os" = "Linux" ]; then
+      arch="$(uname -m)"
+      [ -e "$libs/$arch"-linux-gnu/libaio.so.1t64 ] && sudo ln -sf "$libs/$arch"-linux-gnu/libaio.so.1t64 "$libs/$arch"-linux-gnu/libaio.so.1
+    fi
   fi
 }
 

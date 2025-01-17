783c77d669a73a60d1eaf9b39a1b75905c37225b
Shivam Mathur <shivam_jpr@hotmail.com>
2025-01-18 02:22:55 +0530

    Fix sed for macos in patch script

--- src/scripts/extensions/patches/common.sh
+++ src/scripts/extensions/patches/common.sh
@@ -1,7 +1,7 @@
 process_file() {
     local file=$1
-    sed -i '0,/#include.*\(php_lcg.h\|php_mt_rand.h\|php_rand.h\|standard\/php_random\.h\).*/s//\#include <ext\/random\/php_random.h>/' "$file"
-    sed -i '/#include.*\(php_lcg.h\|php_mt_rand.h\|php_rand.h\|standard\/php_random\.h\)/d' "$file"
+    sed -i'' -e '0,/#include.*\(php_lcg.h\|php_mt_rand.h\|php_rand.h\|standard\/php_random\.h\).*/s//\#include <ext\/random\/php_random.h>/' "$file"
+    sed -i'' -e '/#include.*\(php_lcg.h\|php_mt_rand.h\|php_rand.h\|standard\/php_random\.h\)/d' "$file"
 }
 
 export -f process_file

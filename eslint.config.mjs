f97ca00780cda8d3f649f02e7c398eb7c37c245f
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-23 15:41:07 +0530

    Bump dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,22 @@
-import {fixupConfigRules, fixupPluginRules} from '@eslint/compat';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
 import jest from 'eslint-plugin-jest';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
 import js from '@eslint/js';
-import {FlatCompat} from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
 
 export default [
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:prettier/recommended',
-      'prettier'
-    )
-  ),
+  js.configs.recommended,
+  ...typescriptEslint.configs['flat/recommended'],
+  importPlugin.flatConfigs.errors,
+  importPlugin.flatConfigs.warnings,
+  importPlugin.flatConfigs.typescript,
+  prettierRecommended,
+  eslintConfigPrettier,
   {
     plugins: {
-      '@typescript-eslint': fixupPluginRules(typescriptEslint),
       jest
     },
 

2608ba9a1a90f123ac257178bf720ea6167e64b0
Shivam Mathur <shivam_jpr@hotmail.com>
2026-07-27 11:36:12 +0530

    Fix geos patch URLs

--- src/scripts/extensions/patches/geos.sh
+++ src/scripts/extensions/patches/geos.sh
@@ -2,8 +2,8 @@ patch_geos() {
   if [[ $(printf '%s\n%s\n' "${version:?}" "7.0" | sort -V | head -n1) == "7.0" ]]; then
     sed -i~ -e "s/, ce->name/, ZSTR_VAL(ce->name)/; s/ulong /zend_ulong /" geos.c
   fi
-  get -q -n /tmp/php8.patch https://git.remirepo.net/cgit/rpms/php/php-geos.git/plain/0003-add-all-arginfo-and-fix-build-with-PHP-8.patch
-  get -q -n /tmp/toString.patch https://git.remirepo.net/cgit/rpms/php/php-geos.git/plain/0006-fix-__toString-with-8.2.patch
+  get -q -n /tmp/php8.patch https://src.fedoraproject.org/rpms/php-geos/raw/2021845a362752948dd0338920f0a0e6b1031b4d/f/0003-add-all-arginfo-and-fix-build-with-PHP-8.patch
+  get -q -n /tmp/toString.patch https://src.fedoraproject.org/rpms/php-geos/raw/2021845a362752948dd0338920f0a0e6b1031b4d/f/0006-fix-__toString-with-8.2.patch
   patch -p1 < /tmp/php8.patch 2>/dev/null || true
   patch -p1 < /tmp/toString.patch 2>/dev/null || true
 }
